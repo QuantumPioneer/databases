827f14aedcb4ba77f5aefeba2bd37d43ec28df42
Jackson Burns <jwburns@mit.edu>
2023-10-24 21:22:49 -0400

    alpha 1

--- tristate20/tristate20.py
+++ tristate20/tristate20.py
@@ -1,12 +1,9 @@
-import os
+import pickle
 
 # dill, peewee, and tqdm are all single-library dependencies
-import dill
-from peewee import SqliteDatabase, Model, BlobField, TextField
-# from fastlogfileparser.gaussian import fast_gaussian_logfile_parser as fglp
-# from fastlogfileparser.gaussian.fast_gaussian_logfile_parser import ALL_FIELDS
+from peewee import SqliteDatabase, Model, BlobField, TextField, IntegerField, BooleanField, FloatField
 
-# print(ALL_FIELDS)
+
 db = SqliteDatabase(None)  # defer to class instantiation
 
 
@@ -16,25 +13,84 @@ class BaseModel(Model):
 
 
 class Results(BaseModel):
+    id = IntegerField(unique=True)
+    # file from which the data was retrieved
     source = TextField(unique=True)
-    result = BlobField()
+
+    # fields saved as interpretable
+    route_section = TextField(null=True)
+    charge = IntegerField(null=True)
+    multiplicity = IntegerField(null=True)
+    max_steps = IntegerField(null=True)
+    normal_termination = BooleanField(null=True)
+    cpu_time = FloatField(null=True)
+    wall_time = FloatField(null=True)
+    e0_h = FloatField(null=True)
+    hf = FloatField(null=True)
+    zpe_per_atom = FloatField(null=True)
+    e0_zpe = FloatField(null=True)
+    gibbs = FloatField(null=True)
+
+    # array fields saved as Pickles
+    scf = BlobField(null=True)
+    recovered_energy = BlobField(null=True)
+    frequency_modes = BlobField(null=True)
+    frequencies = BlobField(null=True)
+    std_forces = BlobField(null=True)
+    std_xyz = BlobField(null=True)
+    xyz = BlobField(null=True)
+
+    def unpickle(item):
+        try:
+            return pickle.loads(item)
+        except:
+            return None
+
+    def __iter__(self):
+        return iter(
+            (
+                self.source,
+                self.route_section,
+                self.multiplicity,
+                self.multiplicity,
+                self.max_steps,
+                self.normal_termination,
+                self.cpu_time,
+                self.wall_time,
+                self.e0_h,
+                self.hf,
+                self.zpe_per_atom,
+                self.e0_zpe,
+                self.gibbs,
+                Results.unpickle(self.scf),
+                Results.unpickle(self.recovered_energy),
+                Results.unpickle(self.frequency_modes),
+                Results.unpickle(self.frequencies),
+                Results.unpickle(self.std_forces),
+                Results.unpickle(self.std_xyz),
+                Results.unpickle(self.xyz),
+            )
+        )
 
 
 # todo: write a connect/close decorator to wrap database reads
 
+
 class TriState20:
     def __init__(self, db_path):
         db.init(db_path)
+        self.id = 1
 
-    def get_all(self):
+    def get_n_converged(self, n):
         db.connect()
-        out = Results.get()
-        # deserialize
-        source, result = out.source, dill.loads(out.result)
+        query = Results.select().where(Results.id < self.id + n, Results.id >= self.id)
+        out = [tuple(iter(res)) for res in query]
         db.close()
-        return source, result
+        self.id += n
+        return out
 
 
 if __name__ == "__main__":
-    ts20 = TriState20("/home/jackson/Desktop/parser_test.db")
-    print(ts20.get_all())
+    ts20 = TriState20("/home/jackson/dft_data_v2.db")
+    print(ts20.get_n_converged(10))
+    print(ts20.get_n_converged(10))
